9010129d12c1e66a4c1c019c0061e2156d31bcd1
Bagaskorro20 <korroku20@gmai.com>
2026-07-10 09:05:04 +0700

    setup dan membuat model country

---
+++ android/app/src/main/kotlin/com/example/negara/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.negara
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/models/country_model.dart
@@ -0,0 +1,44 @@
+class CountryModel {
+  bool? success;
+  Null data;
+  List<Errors>? errors;
+
+  CountryModel({this.success, this.data, this.errors});
+
+  CountryModel.fromJson(Map<String, dynamic> json) {
+    success = json['success'];
+    data = json['data'];
+    if (json['errors'] != null) {
+      errors = <Errors>[];
+      json['errors'].forEach((v) {
+        errors!.add(new Errors.fromJson(v));
+      });
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['success'] = this.success;
+    data['data'] = this.data;
+    if (this.errors != null) {
+      data['errors'] = this.errors!.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
+}
+
+class Errors {
+  String? message;
+
+  Errors({this.message});
+
+  Errors.fromJson(Map<String, dynamic> json) {
+    message = json['message'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['message'] = this.message;
+    return data;
+  }
+}
